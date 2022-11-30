16a693c9954f8e0e85c14eff7f0cf2c99ae95bb6
philipp.minder <philipp.minder@supsign.ch>
2022-11-30 17:01:42 +0100

    switch to vite and make demo

---
+++ sandbox/App.vue
@@ -0,0 +1,23 @@
+<!-- sandbox/App.vue -->
+<template>
+  <main>
+    <h1>Welcome to your your sandbox environment</h1>
+
+    <Input
+      label="firstname"
+      :input-attributes="{
+        placeholder: 'Enter your firstname...',
+        type: 'text',
+        autocomplete: 'given-name',
+      }"
+    >
+      First-name
+    </Input>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Input from "../src/Input.vue";
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// src/index.ts
+import Input from './Input.vue'
+
+export { Input }
