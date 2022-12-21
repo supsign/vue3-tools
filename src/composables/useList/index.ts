81c1386dd55c2e640336701faea12a4c4f5f5c1b
philipp.minder <philipp.minder@supsign.ch>
2022-12-21 16:58:40 +0100

    fix types and update fn

--- src/composables/useList/index.ts
+++ src/composables/useList/index.ts
@@ -1,35 +1,39 @@
-import { ref, type Ref } from 'vue';
+import { reactive, ref, type Ref, unref  } from 'vue';
 
-export function useList<T>(data: T[]) {
+export function useList<T = any>(data: T[]) {
 
-  const list: Ref<T[]> = ref(data) as Ref<T[]>;
-
+  const list: T[] = data;
   const selected: Ref<T|undefined> = ref();
+  const selectedIndex: Ref<number|undefined> = ref();
   const show = ref(false);
 
   function add(value: T): void {
-    list.value.push(value);
+    list.push(value);
     show.value = false;
   }
 
-  function update(index: number, value: unknown): void {
-    //@ts-expect-error: find out
-    list.value[index] = value;
+  function update(value: T): void {
+    if(selectedIndex.value) {
+      list[selectedIndex.value] = value;
+      selectedIndex.value = undefined;
+    }
     show.value = false;
     selected.value = undefined;
   }
 
   function remove(index: number): void {
-    list.value.splice(index, 1);
+    list.splice(index, 1);
   }
 
   function select(index: number): void {
-    selected.value = list.value[index];
+    selected.value = list[index];
+    selectedIndex.value = index;
     show.value = true;
   }
 
   function deselect(): void {
     selected.value = undefined;
+    selectedIndex.value = undefined;
     show.value = false;
   }
 
